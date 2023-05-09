d8fb308ddf84b2faa6434a288fbbdda802890c1e
Philipp Nolte <phipno@gmail.com>
2023-05-09 17:49:09 +0200

    added MLX42

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/08 17:11:30 by pnolte            #+#    #+#              #
-#    Updated: 2023/05/08 17:49:17 by pnolte           ###   ########.fr        #
+#    Updated: 2023/05/09 17:46:30 by pnolte           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ CC		:= cc
 
 CFLAGS	:= -I src/ -I lib/ -g3 -Wall -Werror -Wextra
 
-LDFLAGS	:=
+MLXFLA	:= -I include -lglfw -L "/Users/$(USER)/.brew/opt/glfw/lib/"
 
 VPATH	:= src/
 
@@ -32,12 +32,16 @@ LIBFT	:= lib/libft-phipno/libft.a
 
 LIB_P	:= ./lib/libft-phipno
 
+MLX42	:= lib/MLX42/build/libmlx42.a
+
+LIBMLX	:= ./lib/MLX42
+
 #.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~. RULES .~"~._.~"~.__.~"~._.~"~._.~"~._.~"~#
 
 all: $(NAME)
 
-$(NAME): $(OBJ_DIR) $(OBJ) $(LIBFT)
-	$(CC) $(CFLAGS) $(OBJ) -o $(NAME) $(LDFLAGS)
+$(NAME): $(OBJ_DIR) $(OBJ) $(LIBFT) $(MLX42)
+	$(CC) $(CFLAGS) $(OBJ) $(MLXFLA) $(MLX42) $(LIBFT) -o $(NAME)
 
 $(OBJ_DIR):
 	mkdir obj
@@ -45,12 +49,17 @@ $(OBJ_DIR):
 $(OBJ_DIR)%.o: %.c
 	$(CC) $(CFLAGS) $< -c -o $@
 
+$(MLX42):
+	cmake -B build $(LIBMLX)
+	cmake --build build -j4 $(LIBMLX)
+
 $(LIBFT):
 	make -C $(LIB_P)
 
 clean:
 	make -C $(LIB_P) clean
 	rm -rf $(OBJ_DIR)
+	rm -rf $(LIBMLX)/build
 
 fclean: clean
 	make -C $(LIB_P) fclean
@@ -58,7 +67,7 @@ fclean: clean
 
 re: fclean all
 
-.PHONY: all, clean, fclean, re, libft
+.PHONY: all, clean, fclean, re, libft, MLX42,
 
 #.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~#
 # **************************************************************************** #
--- src/main.c
+++ src/main.c
@@ -6,22 +6,93 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 17:15:21 by pnolte            #+#    #+#             */
-/*   Updated: 2023/05/08 17:44:35 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/05/09 17:44:38 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //alle includes die der File braucht oben in den File
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "lib/MLX42/include/MLX42/MLX42.h"
 
-#include "inc/cub3d.h"
+#define WIDTH 512
+#define HEIGHT 512
 
-int main() {
+static mlx_image_t* image;
 
-	while (42)
+// -----------------------------------------------------------------------------
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+
+void ft_randomize()
+{
+	for (uint32_t i = 0; i < image->width; ++i)
 	{
-		exit(EXIT_SUCCESS);
+		for (uint32_t y = 0; y < image->height; ++y)
+		{
+			uint32_t color = ft_pixel(
+				rand() % 0xFF, // R
+				rand() % 0xFF, // G
+				rand() % 0xFF, // B
+				rand() % 0xFF  // A
+			);
+			mlx_put_pixel(image, i, y, color);
+		}
 	}
+}
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		image->instances[0].x += 5;
+}
+
+// -----------------------------------------------------------------------------
+
+int32_t main()
+{
+	mlx_t* mlx;
+
+	// Gotta error check this stuff
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, 128, 128)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+
+	mlx_loop_hook(mlx, ft_randomize, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
 
 /* ************************************************************************** */
