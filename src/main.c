9f44857cf29d7fce19681914757c1a92ee9bc03c
jweeeezy <jwillert@42heilbronn.student.de>
2023-05-24 10:43:55 +0200

    new lib update

--- src/main.c
+++ src/main.c
@@ -6,22 +6,18 @@
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 17:15:21 by pnolte            #+#    #+#             */
-/*   Updated: 2023/05/23 19:48:34 by jwillert         ###   ########.fr       */
+/*   Updated: 2023/05/24 10:29:24 by jwillert         ###   ########          */
 /*                                                                            */
 /* ************************************************************************** */
 
-//alle includes die der File braucht oben in den File
+#include <unistd.h>		// needed for MACROS
+#include "MLX42.h"		// needed for MACROS
+#include "libft.h"		// needed for ft_putstr_fd()
+#include "cub3d.h"		// needed for t_all
+#include "ft_printf.h"	// needed for ft_printf()
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include "MLX42.h"
-#include "libft.h"
-#include "cub3d.h"
-#include "ft_printf.h"
-
-int	main(int argc, char *argv[]) {
+int	main(int argc, char *argv[])
+{
 	t_all all;
 	int	status;
 
@@ -31,7 +27,7 @@ int	main(int argc, char *argv[]) {
 		return (EXIT_FAILURE);
 	}
 	status = cub_map_muncher(&all, argv[1]);
-	ft_printf(2, "Exited with status: %d\n", status);
+	ft_printf(STDERR_FILENO, "Exited with status: %d\n", status);
 	return (status);
 }
 
