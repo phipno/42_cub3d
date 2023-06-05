d73396e68895f17dd671518a65006c0882bdfb1f
jweeeezy <jwillert@42heilbronn.student.de>
2023-06-05 19:04:57 +0200

    fixed norm in hook.c

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:28:07 by jwillert          #+#    #+#             */
-/*   Updated: 2023/06/05 12:51:17 by jwillert         ###   ########          */
+/*   Updated: 2023/06/05 19:02:43 by jwillert         ###   ########          */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,45 @@
 #include "cub3d.h"  // needed for t_all
 #include <unistd.h> // needed for STDERR_FILENO
 
-static void	handle_m(t_all *all)
+static void	toggle_minimap_fullscreen(t_all *all)
+{
+	mlx_delete_image(all->mlx, all->image_minimap);
+	all->image_minimap = mlx_new_image(all->mlx, WIDTH, HEIGHT);
+	if (all->image_minimap == NULL)
+	{
+		// @todo handle error
+		mlx_terminate(all->mlx);
+		cub_exit(EXIT_FAILURE, STDERR_FILENO, "image_minimap init");
+	}
+	all->image_minimap->enabled = true;
+	minimap_init(all->minimap, all->map.map_column_max,
+		all->map.map_line_max, MODE_FULLSCREEN);
+	minimap_draw(all->map.a_map, all->image_minimap, *all->minimap);
+}
+
+static void	toggle_minimap_corner(t_all *all)
+{
+	mlx_delete_image(all->mlx, all->image_minimap);
+	all->image_minimap = mlx_new_image(all->mlx, WIDTH, HEIGHT);
+	if (all->image_minimap == NULL)
+	{
+		// @todo handle error
+		mlx_terminate(all->mlx);
+		cub_exit(EXIT_FAILURE, STDERR_FILENO, "image_minimap init");
+	}
+	minimap_init(all->minimap, all->map.map_column_max,
+		all->map.map_line_max, MODE_CORNER);
+	minimap_draw(all->map.a_map, all->image_minimap, *all->minimap);
+}
+
+static void	toggle_minimap(t_all *all)
 {
 	static size_t	i;
 
 	i += 1;
-	if (i == 1)	
+	if (i == 1)
 	{
-		mlx_delete_image(all->mlx, all->image_minimap);
-		all->image_minimap = mlx_new_image(all->mlx, WIDTH, HEIGHT);
-		if (all->image_minimap == NULL)
-		{
-			// @todo handle error
-			mlx_terminate(all->mlx);
-			cub_exit(EXIT_FAILURE, STDERR_FILENO, "image_minimap init");
-		}
-		minimap_init(all->minimap, all->map.map_column_max, 
-				all->map.map_line_max, MODE_CORNER);
-		minimap_draw(all->map.a_map, all->image_minimap, *all->minimap);
+		toggle_minimap_corner(all);
 	}
 	else if (i == 2)
 	{
@@ -40,18 +61,7 @@ static void	handle_m(t_all *all)
 	else if (i == 3)
 	{
 		i = 0;
-		mlx_delete_image(all->mlx, all->image_minimap);
-		all->image_minimap = mlx_new_image(all->mlx, WIDTH, HEIGHT);
-		if (all->image_minimap == NULL)
-		{
-			// @todo handle error
-			mlx_terminate(all->mlx);
-			cub_exit(EXIT_FAILURE, STDERR_FILENO, "image_minimap init");
-		}
-		all->image_minimap->enabled = true;
-		minimap_init(all->minimap, all->map.map_column_max, 
-				all->map.map_line_max, MODE_FULLSCREEN);
-		minimap_draw(all->map.a_map, all->image_minimap, *all->minimap);
+		toggle_minimap_fullscreen(all);
 	}
 	if (i != 2)
 	{
@@ -75,6 +85,6 @@ void	hook_keys(mlx_key_data_t key_data, void *context)
 	}
 	else if (mlx_is_key_down(all->mlx, MLX_KEY_M) == true)
 	{
-		handle_m(all);
+		toggle_minimap(all);
 	}
 }
