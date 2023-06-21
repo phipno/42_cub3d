a542e228491ddf853d8f83db6cb770ed87b52d4c
jweeeezy <jwillert@42heilbronn.student.de>
2023-06-21 14:01:10 +0200

    found small error not null terminating after read

--- src/valid_map.c
+++ src/valid_map.c
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 16:10:22 by pnolte            #+#    #+#             */
-/*   Updated: 2023/06/21 13:04:14 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/06/21 14:00:33 by jwillert         ###   ########          */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,12 +65,10 @@ int	map_valid_question_mark(t_game *a)
 	{
 		if (nono_wrong_characters(a->a_map[line]) == true)
 		{
-			printf("hello\n");
 			return (EXIT_FAILURE);
 		}
 		if (im_the_logicchecker(a, line) == true)
 		{
-			printf("hi\n");
 			return (EXIT_FAILURE);
 		}
 		line++;
